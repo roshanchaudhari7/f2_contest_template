0e96e255e924899559dc33585734e7f9ce6a16f6
roshanchaudhari7 <roshanchaudhari1872002@gmail.com>
2024-07-20 20:19:12 +0530

    Code is Updated

--- script.js
+++ script.js
@@ -8,20 +8,44 @@ let arr = [
 
 function PrintDeveloperbyMap() {
   //Write your code here , just console.log
+  arr.map(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(employee => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(newEmployee);
 }
 
 function removeAdmin() {
   //Write your code here, just console.log
+  arr = arr.filter(employee => employee.profession !== "admin");
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArray = [
+    { id: 5, name: "kunal", age: "26", profession: "developer" },
+    { id: 6, name: "pavan", age: "23", profession: "admin" },
+    { id: 7, name: "raj", age: "20", profession: "developer" },
+  ];
+
+  let concatinatedArr = arr.concat(newArray);
+  console.log(concatinatedArr);
 }
+
